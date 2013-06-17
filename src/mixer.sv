82c8daf6529f880ab52646d237c07c0d5ca9b4a1
mchevalier <mathieu.chevalier@telecom-paristech.fr>
2013-06-17 15:41:27 +0200

    Nouveau module mixer

---
+++ src/mixer.sv
@@ -0,0 +1,20 @@
+module mixer(input logic          blank,
+             input logic [7:0]  bck_r,
+             input logic [7:0]  bck_g,
+             input logic [7:0]  bck_b,
+             output logic [9:0] vga_r,
+             output logic [9:0] vga_g,
+             output logic [9:0] vga_b);
+
+   // Vérifie qu'on est dans la zone active, sinon, c'est noir
+   always@(*)
+     if(blank)
+       begin
+          vga_r <= {bck_r,bck_r[0]*4};
+          vga_b <= {bck_b,bck_b[0]*4};
+          vga_g <= {bck_g,bck_g[0]*4};
+       end
+     else
+       {vga_r,vga_b,vga_g} <= 0;
+
+endmodule
