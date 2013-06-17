cb81dfedea689265386649339139910f57e0c5be
mchevalier <mathieu.chevalier@telecom-paristech.fr>
2013-06-17 16:48:10 +0200

    test mire horizontale2

--- src/mixer.sv
+++ src/mixer.sv
@@ -1,13 +1,13 @@
-module mixer(input logic          blank,
-             input logic [7:0]  bck_r,
-             input logic [7:0]  bck_g,
-             input logic [7:0]  bck_b,
+module mixer(input logic        blank,
+             input logic  [7:0] bck_r,
+             input logic  [7:0] bck_g,
+             input logic  [7:0] bck_b,
              output logic [9:0] vga_r,
              output logic [9:0] vga_g,
              output logic [9:0] vga_b);
 
    // Vérifie qu'on est dans la zone active, sinon, c'est noir
-   always@(*)
+   always @(*)
      if(blank)
        begin
           vga_r <= {bck_r,bck_r[0]*4};
