f5a75309b95a593f8b285f6667f29b26cadba8bf
Adèle Hurabielle <adele.hurabielle@telecom-paristech.fr>
2013-06-18 17:37:49 +0200

    prototype de clavier 2 a demain

--- src/keyboard.sv
+++ src/keyboard.sv
@@ -1,11 +1,28 @@
-module keyboard( input  clk,
-                 input  reset_n,
-                 input  ps2_data,
-                 output data_out
+`default_nettype none
+module keyboard( input  logic clk,
+                 input  logic reset_n,
+                 input  logic ps2_clk,
+                 input  logic ps2_data,
+                 output logic data_out,
+                 output logic data_valide
                  );
    // buffer
-   logic [8:0]          buffer;
+   logic [10:0]          buffer;
    logic                parity;
+
+   // Dé-métastabilisateur
+   logic                ps2_clk_r, ps2_clk_clean;
+   logic                ps2_data_r, ps2_data_clean;
+
+   always @(posedge clk)
+     begin
+        ps2_clk_r  <=  ps2_clk_clean;
+        ps2_data_r <= ps2_data_clean;
+        ps2_clk  <=  ps2_clk_r;
+        ps2_data <= ps2_data_r;
+     end
+
+   //
 
    //bit de parite
    always @(*)
@@ -13,18 +30,25 @@ module keyboard( input  clk,
 
 
    //envoie
-   always @(posedge clk or negedge reset_n)
+   always @(posedge ps2_clk_clean or negedge reset_n)
      if(~reset_n)
        begin
           data_out <= 0;
           buffer   <= 0;
+          data_valide <= 0;
        end
      else
        begin
           if (((~compt) == 10) && ((~compt) == 0)))
-           {buffer} <= {buffer[7:0],ps2_data};
-           if((parity == buffer[8]) && (compt == 10))
-            data_out <= buffer[7:0];
+           {buffer} <= {buffer[9:0],ps2_data_clean};
+           if((parity == buffer[1]) && (compt == 10) && (buffer[0]) && (~buffer[10]))
+            begin
+               data_out <= buffer[9:2];
+               data_valide <= 1;
+            end
+           else
+             data_valide <= 0;
+
 
        end // else: !if(~reset_n)
 
