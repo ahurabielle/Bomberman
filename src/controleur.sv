a9d4621881ef75a8ec7e07f6388a8493ef191f3b
Alexis Polti <alexis.polti@telecom-paristech.fr>
2013-06-22 01:02:40 +0200

    Correction controleur

--- src/controleur.sv
+++ src/controleur.sv
@@ -1,40 +1,40 @@
-module controleur (input                      clk,
-		           input                      reset_n,
-                    // va délimiter le temps durant lequel center pourra etre modifie
-		           input logic                SOF,
-		           input logic                EOF,
+module controleur (input              clk,
+		           input              reset_n,
+                   // va délimiter le temps durant lequel center pourra etre modifie
+		           input logic        SOF,
+		           input logic        EOF,
                    // commandes des joueurs
-                   input logic                j1_up,
-                   input logic                j1_down,
-                   input logic                j1_left,
-                   input logic                j1_right,
-                   input logic                j2_up,
-                   input logic                j2_down,
-                   input logic                j2_left,
-                   input logic                j2_right,
+                   input logic        j1_up,
+                   input logic        j1_down,
+                   input logic        j1_left,
+                   input logic        j1_right,
+                   input logic        j2_up,
+                   input logic        j2_down,
+                   input logic        j2_left,
+                   input logic        j2_right,
                    // correspond au sprite du j1 affiché
-                   output logic [2:0]         player1_num,
+                   output logic [2:0] player1_num,
                    // correspond au sprite du j2 affiché
-                   output logic [2:0]         player2_num,
+                   output logic [2:0] player2_num,
                    // coordonnee du centre
-                   output logic signed [10:0] player1_centerX,
-		           output logic signed [10:0] player1_centerY,
-                   output logic signed [10:0] player2_centerX,
-                   output logic signed [10:0] player2_centerY
+                   output logic [9:0] player1_centerX,
+		           output logic [9:0] player1_centerY,
+                   output logic [9:0] player2_centerX,
+                   output logic [9:0] player2_centerY
 
 		           );
 
    // variables locales
    // est on ou pas dans la trame
-   logic                                      verou_trame ;
+   logic                              verou_trame ;
    // compteur permettant la réduction de vitesse de l'horloge
-   logic [10:0]                               compt;
+   logic [10:0]                       compt;
    // taille de la fenetre active
-   localparam integer                         HACTIVE = 800;
-   localparam integer                         VACTIVE = 600;
+   localparam integer                 HACTIVE = 800;
+   localparam integer                 VACTIVE = 600;
    // compteur permettant de faire marcher les sprites
-   logic [24:0]                                compt_player1;
-   logic [25:0]                                compt_player2;
+   logic [24:0]                       compt_player1;
+   logic [25:0]                       compt_player2;
 
    // instantiation des compteurs qui vont servir a alterner les sprites quand le bonhomme marche
    always @(posedge clk or negedge reset_n)
@@ -76,7 +76,7 @@ module controleur (input                      clk,
           player2_centerX <= 450;
           player2_centerY <= 300;
        end
-    // si le verou est a un et que j ai recu une donnée du clavier alors je bouge
+   // si le verou est a un et que j ai recu une donnée du clavier alors je bouge
      else if(verou_trame)
        begin
           // On conditionne pour que le sprite (32*32) ne sorte pas de la fenetre
@@ -89,7 +89,7 @@ module controleur (input                      clk,
           if(j1_left && (player1_centerX >= 32))
             player1_centerX <= player1_centerX - 1;
           if(j2_up &&  (player2_centerY >= 0))
-             player2_centerY <= player2_centerY - 1;
+            player2_centerY <= player2_centerY - 1;
           if(j2_down && (player2_centerY < VACTIVE-32))
             player2_centerY <= player2_centerY + 1;
           if(j2_right && (player2_centerX < (HACTIVE - 32)))
