a9fd02f40112484bc853b8a5d500e3c57805b182
mchevalier <mathieu.chevalier@telecom-paristech.fr>
2013-06-27 14:15:00 +0200

    Cleanup

--- maze/gen_maze.py
+++ maze/gen_maze.py
@@ -9,7 +9,7 @@ import os              #Libairie Fichier
 import sys             #Librairie Systeme
 
 
-def gen_maze(filename):
+def gen_maze(filename, fd):
     """
     Génération du plan.
     Comme elle est la meme pour toutes les images, on la prend pour le plan dont le nom est
@@ -18,10 +18,6 @@ def gen_maze(filename):
 
     #On ouvre le fichier dont le nom a été tapé en entrée
     fs = open(filename, 'r')
-
-    #On crée le fichier carte correspondant
-    basename, extension = os.path.splitext(filename)
-    fd = open("%s.lst"%basename, "w")
 
     # On y stocke une donnée par ligne qui corespond à un nombre entre
     # 1 et 6
@@ -33,7 +29,7 @@ def gen_maze(filename):
         # Vérifie que la ligne fait bien 25 caractères de large
         line = line.strip("\r\n")
         if len(line) != 25:
-            raise Exception("La ligne %s ne contient pas 25 caractères"%line)
+            raise Exception("La ligne %s ne contient pas 25 caractères" %line)
 
         # Lit les caractère de la ligne courante un par un, et génère le fichier de sortie
         for txt in line:
@@ -47,14 +43,19 @@ def gen_maze(filename):
             if txt == "1" : fd.write("7\n")
             if txt == "2" : fd.write("8\n")
 
+
         # On comble par 7 octets à 0 (==sprite vide) pour aligner les lignes sur
         # des frontières de 8 mots
         for i in range(7):
             fd.write("0\n")
 
+    # On comble par 15*32 octets à 0 (==sprite vide) pour aligner les colonnes sur
+    # des frontières de 8 mots
+    for i in range(15*32):
+        fd.write("0\n")
+
     # Fermeture des fichiers
     fs.close()
-    fd.close()
     return
 
 def usage():
@@ -74,10 +75,15 @@ def main():
         usage()
         sys.exit(-1)
 
+    # on ouvre le fichier de sortie
+    fd = open("maze.lst", "w")
+
     # Génération de la carte
     for name in sys.argv[1:]:
-        gen_maze(name)
+        gen_maze(name, fd)
 
+    # On ferme de fichier de sortie
+    fd.close()
 
 if __name__ == "__main__":
     main()
